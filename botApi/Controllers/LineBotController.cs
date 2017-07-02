6c3f5f15d54dabea370faaccb000aeb98ad7a62b
黃曼君 <wind04181986@gmail.com>
2017-07-02 14:29:01 +0800

    刪除不必要的程式碼

--- botApi/Controllers/LineBotController.cs
+++ botApi/Controllers/LineBotController.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Web.Http;
-using System.Web.Http.Description;
 using botApi.Models;
 
 namespace botApi.Controllers
@@ -14,55 +8,21 @@ namespace botApi.Controllers
     public class LineBotController : ApiController
     {
         BotRepository db = new BotRepository();
-               // GET api/values
+              
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        public HttpResponseMessage Get(string message)
-        {
-            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "value");
-            response.Content = new StringContent(message, Encoding.Unicode,"text/html");
-            response.Headers.CacheControl = new CacheControlHeaderValue()
-            {
-                MaxAge = TimeSpan.FromMinutes(20)
-            };
-            return response;
-        }
-
-        //// POST api/values
-        //public string PostLineBot([FromBody]string value)
-        //{
-        //    return value;
-        //}
-
-        //[HttpPost]
-        //[ActionName("PostMessage")]
         public IHttpActionResult Post() 
         {
             try
             {
                 string ChannelAccessToken = db.GetBotToken("Line");
                 string postData = Request.Content.ReadAsStringAsync().Result;
-                if (db.InsertRequestLog(postData))
-                {
-                    var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
-                    //會有exception
-                    string Message = "你說了:" + ReceivedMessage.events[0].message.text;
-                    isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
-                }
-                else
-                {
-                    //TODO 寫log
-                }
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                string Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
                 return Ok();
             }
             catch (Exception ex)
@@ -74,20 +34,6 @@ namespace botApi.Controllers
                 db.InsertRequestLog(errorMessage);
                 return Ok(errorMessage);
             }
-           
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-
-
-
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
         }
     }
 }
