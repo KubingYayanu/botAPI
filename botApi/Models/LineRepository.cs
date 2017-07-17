0deee9cfe2acb94efd552262ee77aeb696654164
黃曼君 <wind04181986@gmail.com>
2017-07-18 00:35:01 +0800

    修正TSQL

--- botApi/Models/LineRepository.cs
+++ botApi/Models/LineRepository.cs
@@ -64,7 +64,7 @@ namespace botApi.Models
 SELECT A.*
   FROM dbo.BotTemplateMessage A
  INNER JOIN dbo.BotData B 
-    ON A.BotSeq = B.BotSeq
+    ON A.BotSeq = BotSeq
  WHERE B.BotName = 'LINE' 
    AND A.Type = 'Menu'" ,  "");
 
